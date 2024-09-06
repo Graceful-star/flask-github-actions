2adc5f0c022c1186422e3ae29922d0e8d9c66477
Graceful-star <103196746+Graceful-star@users.noreply.github.com>
2024-09-06 20:31:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+# app.config['TESTING'] = True 
+
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, !</p>"
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
 from flask import Flask
 
 app = Flask(__name__)
-app.config['TESTING'] = True 
+app.config['TESTING'] = True
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, !</p>"
+    return "Hello, World!"
 
 if __name__ == "__main__":
     app.run(debug=True)
